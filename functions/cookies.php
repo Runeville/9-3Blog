e6faa842599e179c27ad583ffe9cab322ed6bfe5
Rune <varkhipov2004@gmail.com>
2020-01-03 19:45:50 +0600

    Revert "Shared all local files to a project"
    This reverts commit e7ba893a6ff1b510af1f4986e5a7a261bb7ca07d.

--- functions/cookies.php
+++
@@ -1,9 +0,0 @@
-<?php
-if($_GET['auth']){
-    setcookie('login', '', time() - 3600);
-    echo '<script>location="../authorisation.php"</script>';
-}
-
-if(empty($_COOKIE['login'])){
-    echo '<script>location="authorisation.php"</script>';
-}
